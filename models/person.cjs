daafaa2fed01a5a436712d7127eb66415a4ed23f
Magdiel Mora <m41k80@icloud.com>
2024-10-29 07:59:27 -0500

    update

--- models/person.cjs
+++ models/person.cjs
@@ -23,11 +23,12 @@ const personSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: function(v) {
-        return /^\d{2,3}-\d+{5,}/.test(v)
+        return /^\d{2,3}-\d{7,8}/.test(v)
       },
       message: props => `${props.value} is not a valid phone number!`
     },
     required: [true, 'User phone number required'],
+    minlength: 8
   }
 })
 
