104795493cd08d985003858af0aeff01d458abb6
Magdiel Mora <m41k80@icloud.com>
2024-10-29 08:40:27 -0500

    error frontend

--- index.cjs
+++ index.cjs
@@ -29,6 +29,8 @@ app.use(cors())
 app.use(express.json())
 // app.use(morgan('tiny'))
 
+app.use(express.static('dist'))
+
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
